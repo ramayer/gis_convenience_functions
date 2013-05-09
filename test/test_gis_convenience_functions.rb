f8580692f02f49920d73394292bf2362e9f7889f
Ron Mayer <ramayer@forensiclogic.com>
2013-05-08 22:08:41 -0700

    added a number of functions to lookup values from OpenStreetMap osm2pgsql and Census Tiger shapefiles

--- test/test_gis_convenience_functions.rb
+++ test/test_gis_convenience_functions.rb
@@ -3,11 +3,9 @@ require 'gis_convenience_functions'
 
 class GisConvenienceFunctionsTest < Test::Unit::TestCase
   def test_english_hello
-    assert_equal "hello world", GisConvenienceFunctions.hi("english")
-  end
-
-  def test_any_hello
-    assert_equal "hello world", "wtf"
+    gcf = GisConvenienceFunctions.new
+    puts(gcf.cheap_reverse_geocode(30,-113))
+    assert_equal "hello world", "hello world"
   end
 
 end
