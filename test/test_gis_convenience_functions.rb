af468ae156cf32154cbc8091f9db2508a9aa4557
Ron Mayer <ramayer+git@gmail.com>
2016-05-17 13:49:33 -0700

    separate environment variables for test script

--- test/test_gis_convenience_functions.rb
+++ test/test_gis_convenience_functions.rb
@@ -3,7 +3,12 @@ require 'gis_convenience_functions'
 
 class GisConvenienceFunctionsTest < Test::Unit::TestCase
   def test_english_hello
-    gcf = GisConvenienceFunctions.new
+    gcf = GisConvenienceFunctions.new(ENV['GIS_USER'] || 'gis',
+                                  ENV['GIS_DATABASE'] || 'gis',
+                                  ENV['GIS_HOST'] || 'gis_db',
+                                  ENV['GIS_PORT'] || '5432',
+                                  ENV['GIS_PASSWORD'])
+
     puts(gcf.cheap_reverse_geocode(30,-113))
     assert_equal "hello world", "hello world"
   end
